fc79436b1b036972ebeaf105d97115ed80fbaccd
anungyudha <anungyudhap@gmail.com>
2025-08-24 22:31:58 +0700

    revision: detail makanan page

--- src/app/resep-makanan/[id]/page.tsx
+++ src/app/resep-makanan/[id]/page.tsx
@@ -49,58 +49,65 @@ export default function FoodRecipeDetails({ params }: FoodRecipeDetailsProps) {
       <p className="mt-6 text-justify leading-7 text-gray-700">
         {data.description}
       </p>
-      <div className="mt-10 grid md:grid-cols-2 gap-8">
 
+
+      <div className="mt-10 grid md:grid-cols-2 gap-8">
+<div className="space-y-8">
+      <div>
+        <h3 className="inline-block bg-primary px-6 py-2 rounded-lg font-semibold text-white mb-4">
+          Bahan
+        </h3>
+        <ul className="list-disc ml-1 space-y-2 text-gray-700">
+          <p>75 gr singkong putih, rebus dan haluskan </p>
+          <p>15 gr (2 sdm datar) daging ikan kembung cincang halus  </p>
+          <p>15 gr daging ayam cincang rebus </p>
+          <p>250 ml air kaldu ayam </p>
+          <p>5 gr (1 sdt) minyak kelapa </p>
+          <p>20 gr (2 sdm) bayam segar, potong halus </p>
+        </ul>
+      </div>
+
+      
+    </div>
   <div>
     <h3 className="inline-block bg-primary px-6 py-2 rounded-lg font-semibold text-white mb-4">
       Cara Masak
     </h3>
-    <ul className="list-disc ml-5 space-y-2 text-gray-700">
-      <li>Tumis bumbu halus, lalu masukkan daun salam dan sereh.</li>
-      <li>Tambahkan air kaldu, masukkan singkong putih, daging ikan, daging ayam cincang rebus, aduk-aduk hingga setengah matang.</li>
-      <li>Masukkan daun bayam, aduk hingga matang.</li>
-      <li>Jika airnya mengental dapat ditambahkan air matang.</li>
-      <li>Angkat, lalu saring halus atau diblender.</li>
-      <li>Sebelum disajikan tambahkan saus jeruk.</li>
-    </ul>
+    <div className="space-y-4 text-gray-700">
+  <div className="flex items-start">
+    <span className="flex h-6 w-6 shrink-0 items-center justify-center rounded-full bg-gray-300 text-sm font-medium text-white mr-3">
+      1
+    </span>
+    <p>Rebus air, masukkan ayam dengan daun salam, sereh, daun jeruk, dan lengkuas hingga mendidih.</p>
+  </div>
+
+  <div className="flex items-start">
+    <span className="flex h-6 w-6 shrink-0 items-center justify-center rounded-full bg-gray-300 text-sm font-medium text-white mr-3">
+      2
+    </span>
+    <p>Tumis bumbu halus sampai harum, masukkan ke dalam air rebusan.</p>
+  </div>
+
+  <div className="flex items-start">
+    <span className="flex h-6 w-6 shrink-0 items-center justify-center rounded-full bg-gray-300 text-sm font-medium text-white mr-3">
+      3
+    </span>
+    <p>Angkat ayam, lalu suwir ayam.</p>
+  </div>
+
+  <div className="flex items-start">
+    <span className="flex h-6 w-6 shrink-0 items-center justify-center rounded-full bg-gray-300 text-sm font-medium text-white mr-3">
+      4
+    </span>
+    <p>Sajikan nasi dengan suwir ayam, soun, tauge, telur puyuh, dan kuah soto. Taburi daun bawang, seledri, dan bawang goreng di atasnya.</p>
+  </div>
+</div>
+
+
   </div>
 
   
-  <div className="space-y-8">
-    <div>
-      <h3 className="inline-block bg-primary px-6 py-2 rounded-lg font-semibold text-white mb-4">
-        Bahan
-      </h3>
-      <ul className="list-disc ml-5 space-y-2 text-gray-700">
-        <li>75 gr singkong putih, rebus dan haluskan</li>
-        <li>15 gr daging ikan kembung cincang halus</li>
-        <li>15 gr daging ayam cincang rebus</li>
-        <li>250 ml air kaldu ayam</li>
-        <li>5 gr minyak kelapa</li>
-        <li>20 gr bayam segar, potong halus</li>
-      </ul>
-    </div>
-
-    <div>
-      <h3 className="inline-block bg-primary px-6 py-2 rounded-lg font-semibold text-white mb-4">
-        Bumbu
-      </h3>
-      <ul className="list-disc ml-5 space-y-2 text-gray-700">
-        <li>1 lembar daun salam</li>
-        <li>1 batang sereh</li>
-      </ul>
-    </div>
-
-    <div>
-      <h3 className="inline-block bg-primary px-6 py-2 rounded-lg font-semibold text-white mb-4">
-        Bumbu Halus
-      </h3>
-      <ul className="list-disc ml-5 space-y-2 text-gray-700">
-        <li>1 siung bawang merah</li>
-        <li>1 siung bawang putih</li>
-      </ul>
-    </div>
-  </div>
+    
 </div>
 
     </div>
